bddf73665223125f8d8bb81633a2cb3fbbd8c207
Ailiy <alaimanramli6@gmail.com>
2025-09-17 16:36:24 +0800

    feature(models): add user schema (#3)

---
+++ src/models/user.models.js
@@ -0,0 +1,64 @@
+/* 
+_id string pk
+watchHistory ObjectId[] videos
+username string
+email string
+fullName string
+avatar string
+coverimage string
+password string
+refreshToken string
+createdAt Date
+updateAt Date
+*/
+
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    fullname: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+    },
+    avatar: {
+      type: String, //cloudinary URL
+      required: true,
+    },
+    coverimage: {
+      type: String, //cloudinary URL
+    },
+    watchHistory: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+    password: {
+      type: String,
+      required: [true, "password is required"],
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+export const User = mongoose.model("User", userSchema);
