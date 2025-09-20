cfcef495a9d9d4ee9e18b784a8aa581519b5b320
Ailiy <alaimanramli6@gmail.com>
2025-09-21 01:21:34 +0800

    feature/auth middleware (#16)
    * feat(auht): add auth.middleware.js into folder middlewares
    
    * feat(auth): add logout method in user.controller.js
    
    * feat(auth): update route in user.routes.js

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -176,10 +176,26 @@ const loginUser = asyncHandler(async (req, res) => {
 });
 
 const logoutUser = asyncHandler(async (req, res) => {
-  await User
-    .findByIdAndUpdate
-    // TODO:need to come back after middleware
-    ();
+  await User.findByIdAndUpdate(
+    req.user._id,
+    {
+      $set: {
+        refreshToken: undefined,
+      },
+    },
+    { new: true }
+  );
+
+  const options = {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+  };
+
+  return res
+    .status(200)
+    .clearCookie("accessToken", options)
+    .clearCookie("refreshToken", options)
+    .json(new ApiResponse(200, {}, "User logged out successfully"));
 });
 
 const refreshAccessToken = asyncHandler(async (req, res) => {
@@ -232,4 +248,4 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
   }
 });
 
-export { registerUser, loginUser, refreshAccessToken };
+export { registerUser, loginUser, refreshAccessToken, logoutUser };
